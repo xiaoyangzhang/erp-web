e38e6820c7df151ba7697edfb23ab1c45a994f43
wangjun <wangjun@jiuxiulvxing.com>
2017-01-16 18:09:06 +0800

    1

--- run/erp_web.prod.bat
+++ run/erp_web.prod.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0..
 call mvn package -DskipTests
 
-call autoconfig %~dp0..\target\erp-web.war -u %~dp0erp_web.prod.properties
+call autoconfig %~dp0..\target\ROOT.war -u %~dp0erp_web.prod.properties
 @pause
