03d88fbc0f7e34f37409448cc40e5e244d39e370
zhangxy <xiaoyangzhang1990@163.com>
2017-03-02 13:24:30 +0800

    add try

--- src/main/java/com/yihg/erp/controller/sys/security/VerifyCodeController.java
+++ src/main/java/com/yihg/erp/controller/sys/security/VerifyCodeController.java
@@ -23,50 +23,51 @@ import com.yihg.erp.utils.MD5Util;
 public class VerifyCodeController {
 	
 	@RequestMapping(value = "verify.htm")  
-	public void getVerifyImage(HttpServletRequest request, HttpServletResponse response) throws Exception {  
-		// 在内存中创建图象
-		int width = 85, height = 20;
-		BufferedImage image = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_RGB);
-		// 获取图形上下文
-		Graphics g = image.getGraphics();
-		// 生成随机类
-		Random random = new Random();
-		// 设定背景色
-		g.setColor(getRandColor(200, 250));
-		g.fillRect(0, 0, width, height);
-		// 设定字体
-		g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		// 随机产生155条干扰线，使图象中的认证码不易被其它程序探测到
-		g.setColor(getRandColor(160, 200));
-		for (int i = 0; i < 155; i++) {
-			int x = random.nextInt(width);
-			int y = random.nextInt(height);
-			int xl = random.nextInt(12);
-			int yl = random.nextInt(12);
-			g.drawLine(x, y, x + xl, y + yl);
-		}
-		// 取随机产生的认证码(6位数字)
-		String sRand = "";
-		for (int i = 0; i < 4; i++) {
-			String rand = String.valueOf(random.nextInt(10));
-			sRand += rand;
-			// 将认证码显示到图象中
-			g.setColor(new Color(20 + random.nextInt(110), 20 + random
-					.nextInt(110), 20 + random.nextInt(110)));
-			// 调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成
-			g.drawString(rand, 13 * i + 6, 16);
-		}		
-		
-		Cookie cookie = new Cookie(SecurityConstant.USER_LOGIN_VERIFY_CODE_KEY,MD5Util.MD5(sRand));
-	    cookie.setPath("/");
-	    //cookie.setMaxAge(24*60*60);
-	    response.addCookie(cookie);
-		
-		// 图象生效
-		g.dispose();
-		ServletOutputStream out = response.getOutputStream();  
+	public void getVerifyImage(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		try {
+			// 在内存中创建图象
+			int width = 85, height = 20;
+			BufferedImage image = new BufferedImage(width, height,
+					BufferedImage.TYPE_INT_RGB);
+			// 获取图形上下文
+			Graphics g = image.getGraphics();
+			// 生成随机类
+			Random random = new Random();
+			// 设定背景色
+			g.setColor(getRandColor(200, 250));
+			g.fillRect(0, 0, width, height);
+			// 设定字体
+			g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			// 随机产生155条干扰线，使图象中的认证码不易被其它程序探测到
+			g.setColor(getRandColor(160, 200));
+			for (int i = 0; i < 155; i++) {
+				int x = random.nextInt(width);
+				int y = random.nextInt(height);
+				int xl = random.nextInt(12);
+				int yl = random.nextInt(12);
+				g.drawLine(x, y, x + xl, y + yl);
+			}
+			// 取随机产生的认证码(6位数字)
+			String sRand = "";
+			for (int i = 0; i < 4; i++) {
+				String rand = String.valueOf(random.nextInt(10));
+				sRand += rand;
+				// 将认证码显示到图象中
+				g.setColor(new Color(20 + random.nextInt(110), 20 + random
+						.nextInt(110), 20 + random.nextInt(110)));
+				// 调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成
+				g.drawString(rand, 13 * i + 6, 16);
+			}
+
+			Cookie cookie = new Cookie(SecurityConstant.USER_LOGIN_VERIFY_CODE_KEY,MD5Util.MD5(sRand));
+			cookie.setPath("/");
+			//cookie.setMaxAge(24*60*60);
+			response.addCookie(cookie);
+
+			// 图象生效
+			g.dispose();
+			ServletOutputStream out = response.getOutputStream();
+
 			ImageOutputStream imageOut = ImageIO.createImageOutputStream(out);
 			ImageIO.write(image, "JPEG", imageOut);
 			imageOut.flush();
